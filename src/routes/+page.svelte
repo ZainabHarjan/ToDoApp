811575c85582d2092599b75545c3aa5f3280dedb
ZainabHarjan <HarjanZ@catlin.edu>
2026-03-14 20:55:41 -0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<center><h1>Welcome to my very kind and cute ToDo Appi!</h1></center>
+
+<style>
+   p {
+        font-family: fantasy;
+        color: darkgoldenrod;
+        align-items: center;
+        size: 16;
+        
+    }
+
+
+
+    h1 {
+        font-family: cursive;
+        color: #fef9ff;
+        border-style: inset;
+        border-color: #fef9ff;
+        border-width: 5px;
+        text-align: center;
+        border-spacing: 10;
+        text-decoration-line: underline;
+        background-color: #ffb5a7;
+        width: 800px;
+        text-size-adjust: 100px;
+
+    }
+
+    body {
+        background-image: url(/flowers.jpg);
+    }
+
+    div {
+        background-color: #ffb5a7;
+        width: 800px;
+        height: 1000px;
+        border-style: inset;
+        border-color: #fef9ff;
+        border-width: 5px;
+        
+    }
+
+</style>
+
+<body>
+
+    <center>
+        <div> 
+            <button>add task</button>
+        </div> 
+    </center>     
+</body>
+
+
+
